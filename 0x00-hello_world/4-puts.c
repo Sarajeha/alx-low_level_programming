be049a3f796ac5a8e0dc312c5bfe50af91057e02
Sarajeha <sarajehd@gmail.com>
2023-03-16 12:54:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+include <stadio.h>
+
+
+/**
+ *  main - prints "Programming is like building a multilingual
+ *   puzzle, followed by a new line."
+ *  return 0
+ */
+
+int main(void)
+
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
